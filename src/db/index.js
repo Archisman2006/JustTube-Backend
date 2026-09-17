4f4c0aaff4358c07cc7d71c87ff11af7654b2262
archisman <archidas2006@gmail.com>
2026-09-17 20:09:40 +0530

    optimize serverless database connection

--- src/db/index.js
+++ src/db/index.js
@@ -1,11 +1,22 @@
 import mongoose from "mongoose";
 import { db_name } from "../constants.js";
-export const connectDB=async ()=>{
-    try {
-        const connectionInstance=await mongoose.connect(`${process.env.MONGODB_URI}/${db_name}`);
-        // console.log(connectionInstance);
-        console.log("\nMONGODB Connected Successfully DB-HOST: "+connectionInstance.connection.host);
-    } catch (error) {
-        console.log("\nMONGODB connection error."); process.exit(1);
+
+let isConnected = false;
+
+export const connectDB = async () => {
+    if (isConnected || mongoose.connection.readyState === 1) {
+        return;
     }
-}
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${db_name}`);
+        isConnected = true;
+        console.log("\nMONGODB Connected Successfully DB-HOST: " + connectionInstance.connection.host);
+    } catch (error) {
+        console.log("\nMONGODB connection error.", error);
+        if (!process.env.VERCEL) {
+            process.exit(1);
+        } else {
+            throw error;
+        }
+    }
+};
