11a03263da3624d3600d8faca06d7bf149806a57
archisman <archidas2006@gmail.com>
2026-09-17 20:05:54 +0530

    add comment field for nested comments and isDeleted for soft delete

--- src/models/comment.model.js
+++ src/models/comment.model.js
@@ -14,21 +14,35 @@ const commentSchema=new mongoose.Schema(
             type: mongoose.Schema.Types.ObjectId,
             ref:'Tweet'
         },
+        comment:{
+            type:mongoose.Schema.Types.ObjectId,
+            ref:'Comment'
+        },
         owner:{
             type: mongoose.Schema.Types.ObjectId,
             ref:'User',required:true        
         },
         isEdited:{
             type:Boolean, required:true
+        },
+        isDeleted:{
+            type:Boolean, default:false, required:true
         }
     },{
         timestamps:true
     }
 )
 commentSchema.pre("validate", function () {
-    if (this.video && this.tweet) {
-        throw new ApiError(400, "Comment must belong to either a video or a tweet, not both.");
+    const parentCount = [this.video, this.tweet, this.comment].filter(Boolean).length;
+    if (parentCount !==1) {
+        throw new ApiError(400, "Comment must belong to exactly one target- video, tweet or comment");
     }
 });
+
+commentSchema.index({ isDeleted: 1 });
+commentSchema.index({ comment: 1 });
+commentSchema.index({ video: 1 });
+commentSchema.index({ tweet: 1 });
+
 commentSchema.plugin(mongooseAggregatePaginate)
 export const Comment=mongoose.model('Comment',commentSchema);
